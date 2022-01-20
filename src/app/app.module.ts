11bb29e499eb4534f25870bd13473233febea743
tanguybro <tanguy.brouassin@gmail.com>
2022-01-20 15:44:32 +0100

    create components and init frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    NavbarComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mock-products.ts
@@ -0,0 +1,16 @@
+import { Product } from './product';
+
+export const PRODUCTS: Product[] = [
+    {
+        id: 1,
+        name: 'tata',
+        image: '../assets/test.jpg',
+        price: 10,
+    },
+    {
+        id: 2,
+        name: 'toto',
+        image: '../assets/test.jpg',
+        price: 50,
+    }
+  ];
