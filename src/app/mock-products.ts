66154e4a5857b7a899c342d55b315dff516eb7c2
tanguybro <tanguy.brouassin@gmail.com>
2022-01-20 16:40:06 +0100

    design products

--- src/app/mock-products.ts
+++ src/app/mock-products.ts
@@ -3,14 +3,26 @@ import { Product } from './product';
 export const PRODUCTS: Product[] = [
     {
         id: 1,
-        name: 'tata',
-        image: '../assets/test.jpg',
+        name: 'Ecouteurs',
+        image: '../assets/ecouteurs.jpg',
         price: 10,
     },
     {
         id: 2,
-        name: 'toto',
-        image: '../assets/test.jpg',
+        name: 'Bouteille mystère',
+        image: '../assets/bouteille_mystere.jpg',
         price: 50,
+    },
+    {
+        id: 3,
+        name: 'Jeu de cartes',
+        image: '../assets/cartes.jpg',
+        price: 30,
+    },
+    {
+        id: 4,
+        name: 'Manga Dofus tome 1 à 7',
+        image: '../assets/manga_dofus.jpg',
+        price: 75,
     }
   ];
