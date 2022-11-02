3de14ab88285b1dcbdde5ba8b4df7e02fc2dc769
tanguybro <tanguy.brouassin@gmail.com>
2022-11-02 14:04:46 +0100

    update products

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,64 +1,64 @@
 import { Product } from './product';
 
 export const PRODUCTS: Product[] = [
-    {
-        id: 1,
-        name: 'Clope',
-        icon: 'smoking',
-        price: 10,
-    },
-    {
-        id: 2,
-        name: '0,0001 BTC',
-        icon: 'bitcoin-sign',
-        price: 10,
-    },
-    {
-        id: 3,
-        name: 'Bonbons',
-        icon: 'candy-cane',
-        price: 15,
-    },
-    {
-        id: 4,
-        name: 'Poster',
-        icon: 'note-sticky',
-        price: 10,
-    },
-    {
-        id: 5,
-        name: 'Jeu d\'échecs',
-        icon: 'chess',
-        price: 80,
-    },
-    {
-        id: 6,
-        name: 'Manga Dofus 1 à 7',
-        icon: 'book',
-        price: 65,
-    },
-    {
-        id: 7,
-        name: 'Ecouteurs',
-        icon: 'headphones',
-        price: 20,
-    },
-    {
-        id: 8,
-        name: 'Montre',
-        icon: 'clock',
-        price: 30,
-    },
-    {
-        id: 9,
-        name: 'Sucette au CBD',
-        icon: 'candy-cane',
-        price: 25,
-    },
-    {
-        id: 10,
-        name: 'Enceintes',
-        icon: 'volume-high',
-        price: 70,
-    }
+  {
+    id: 1,
+    name: 'Clope',
+    icon: 'smoking',
+    price: 10,
+  },
+  {
+    id: 2,
+    name: 'Bonbons',
+    icon: 'candy-cane',
+    price: 15,
+  },
+  {
+    id: 3,
+    name: 'Bouteille',
+    icon: 'wine-bottle',
+    price: 50,
+  },
+  {
+    id: 4,
+    name: '0,0001 BTC',
+    icon: 'bitcoin-sign',
+    price: 10,
+  },
+  {
+    id: 5,
+    name: "Jeu d'échecs",
+    icon: 'chess',
+    price: 60,
+  },
+  {
+    id: 6,
+    name: 'Manga Dofus 1 à 7',
+    icon: 'book',
+    price: 65,
+  },
+  {
+    id: 7,
+    name: 'Chèque vacances',
+    icon: 'money-check',
+    price: 50,
+  },
+  {
+    id: 8,
+    name: 'Montre',
+    icon: 'clock',
+    price: 10,
+  },
+  {
+    id: 9,
+    name: 'Sucette au CBD',
+    icon: 'candy-cane',
+    price: 20,
+  },
+  {
+    id: 10,
+    name: 'GoPro',
+    icon: 'camera',
+    price: 500,
+  },
 ];
